df9866f59704b7423bbe615bdeec292e4733853a
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-16 13:58:40 +0700

    add rand string gen
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rand::Rng;
+use std::{char::from_u32, io};
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("length : ");
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut nums: u32 = 0;
+
+    if let Ok(num) = input.trim().parse::<u32>() {
+        nums = num - 1
+    } else {
+        println!("error is input number ?")
+    }
+
+    for _ in 0..=nums {
+        print!("{}", from_u32(rand::thread_rng().gen_range(33..=126)).unwrap())
+    }
+
 }
